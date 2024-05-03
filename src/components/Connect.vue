459a8cbc5db27d2f747460a6d9d0c788158d6281
kimura <tansen87@qq.com>
2024-05-03 10:15:22 +0800

    refactor(Connect): styles

--- src/components/Connect.vue
+++ src/components/Connect.vue
@@ -10,23 +10,22 @@
   >
     <!-- column 1 -->
     <div class="q-gutter-y-md" style="flex-basis: 23%; max-width: 350px">
-      <q-input color="teal" v-model="data.table" label="Table">
+      <q-input color="teal" v-model="data.table" label="table name">
         <template v-slot:prepend>
           <q-icon name="south_east" />
           <q-select
             v-model="data.sep"
             :options="sepOptions"
-            label="Separator"
-            style="width: 90px"
+            label="sep"
+            style="width: 58px"
           />
         </template>
       </q-input>
     </div>
 
     <!-- column 2 -->
-    <div class="q-gutter-y-md" style="flex-basis: 23%; max-width: 350px">
-      <q-input color="teal" v-model="data.file" label="File"> </q-input>
-      <q-input color="teal" v-model="data.dbFile" label="Database"> </q-input>
+    <div class="q-gutter-y-md" style="flex-basis: 33%; max-width: 500px">
+      <q-input color="teal" v-model="data.file" label="csv file" autogrow> </q-input>
     </div>
 
     <!-- column 3 -->
@@ -47,39 +46,6 @@
           />
         </template>
       </q-input>
-      <q-input>
-        <template v-slot:prepend>
-          <q-btn
-            color="secondary"
-            label="open db"
-            @click="openDBfile"
-            style="width: 145px"
-          />
-          <q-btn
-            color="secondary"
-            label="query"
-            @click="query"
-            style="width: 145px"
-          />
-        </template>
-      </q-input>
-    </div>
-
-    <!-- column 4 -->
-    <div class="q-gutter-y-md" style="flex-basis: 23%; max-width: 350px"></div>
-  </div>
-
-  <div
-    class="q-pa-md"
-    style="
-      display: flex;
-      justify-content: space-between;
-      max-width: 1500px;
-      margin: 0 auto;
-    "
-  >
-    <div class="q-gutter-y-md" style="flex-basis: 80%; max-width: 1200px">
-      <q-input v-model="data.sql" color="teal" label="SQL" autogrow />
     </div>
   </div>
 
@@ -97,7 +63,7 @@
       :rows="tableData"
       :columns="columns"
       row-key="name"
-      style="height: 430px; width: 100%"
+      style="height: 400px; width: 100%"
     >
     </q-table>
   </div>
@@ -115,12 +81,9 @@ const $q = useQuasar();
 const columns = ref([]);
 const tableData = ref([]);
 const data = reactive({
-  table: "duck",
+  table: "gl",
   file: "",
-  fileExtension: ["csv", "txt", "tsv", "spext"],
-  dbFile: "",
-  dbFileExtension: ["duckdb"],
-  sql: "",
+  fileExtension: ["csv", "txt", "tsv", "spext", "dat"],
   sep: ",",
 });
 const sepOptions = [",", "|", "\\t", ";"];
@@ -218,93 +181,6 @@ async function writeDB() {
   } else {
     let notifId = notif({
       message: "写入成功,耗时: " + res,
-      color: "positive",
-      icon: "sentiment_satisfied_alt",
-      spinner: false,
-      actions: [
-        {
-          label: "ok",
-          color: "white",
-          handler: () => {
-            notifId = null;
-          },
-        },
-      ],
-    });
-  }
-}
-
-// open duckdb
-async function openDBfile() {
-  const selected = await open({
-    multiple: false,
-    filters: [
-      {
-        name: "duckdb",
-        extensions: data.dbFileExtension,
-      },
-    ],
-  });
-  if (Array.isArray(selected)) {
-    data.dbFile = selected.toString();
-  } else if (selected === null) {
-    Notify.create({
-      type: "warning",
-      message: "未选择duckdb文件",
-      position: "bottom-right",
-    });
-    return;
-  } else {
-    data.dbFile = selected;
-  }
-}
-
-// excute query
-async function query() {
-  if (data.dbFile === "") {
-    Notify.create({
-      type: "warning",
-      message: "未选择duckdb文件",
-      position: "bottom-right",
-    });
-    return;
-  }
-
-  const notif = $q.notify({
-    color: "ongoing",
-    icon: "ongoing",
-    position: "bottom-right",
-    group: false,
-    timeout: 0,
-    spinner: QSpinnerGears,
-    message: "正在查询...",
-  });
-
-  const res = await invoke("query", {
-    file: data.dbFile,
-    sep: data.sep,
-    sql: data.sql,
-  });
-
-  if (res.includes("Error")) {
-    let notifId = notif({
-      message: res,
-      color: "negative",
-      icon: "sentiment_very_dissatisfied",
-      spinner: false,
-      actions: [
-        {
-          label: "ok",
-          color: "white",
-          handler: () => {
-            notifId = null;
-          },
-        },
-      ],
-    });
-  } else {
-    let notifId = notif({
-      message: "查询成功,耗时: " + res,
       color: "positive",
       icon: "sentiment_satisfied_alt",
       spinner: false,
